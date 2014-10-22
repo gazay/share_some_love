b509330fe7bba78cffd835b0ce1446211b259a00
Alexey Gaziev <alex.gaziev@gmail.com>
2014-10-22 12:22:51 -0800

    Update specs syntax

--- spec/love/basic_spec.rb
+++ spec/love/basic_spec.rb
@@ -6,42 +6,42 @@ describe Love do
 
     it 'should set property by_gemname to true if there arg by_gem' do
       Love.config ['by_gem']
-      Love.by_gemname.should be_true
+      expect(Love.by_gemname).to be_truthy
     end
 
     it 'should set property by_gemname to false if there no arg by_gem' do
       Love.config
-      Love.by_gemname.should be_false
+      expect(Love.by_gemname).to be_falsey
     end
 
     it 'should set property for_site to true if there arg site' do
       Love.config ['site']
-      Love.for_site.should be_true
+      expect(Love.for_site).to be_truthy
     end
 
     it 'should set property for_site to false if there no arg site' do
       Love.config
-      Love.for_site.should be_false
+      expect(Love.for_site).to be_falsey
     end
 
     it 'should set property check_author to true if there arg -v' do
       Love.config ['-v']
-      Love.check_author.should be_true
+      expect(Love.check_author).to be_truthy
     end
 
     it 'should set property check_author to false if there no arg -v' do
       Love.config
-      Love.check_author.should be_false
+      expect(Love.check_author).to be_falsey
     end
 
     it 'should set property root to lib folder' do
       Love.config
-      Love.root.should == Pathname(__FILE__).parent.parent.parent.join('lib')
+      expect(Love.root).to eq(Pathname(__FILE__).parent.parent.parent.join('lib'))
     end
 
     it 'should set property lang to en' do
       Love.config
-      Love.lang.should == 'en'
+      expect(Love.lang).to eq('en')
     end
 
   end
@@ -59,7 +59,7 @@ describe Love do
 
     it 'should ask user to login and create octokit client' do
       Love.login_user
-      Love.octokit.class.should == Octokit::Client
+      expect(Love.octokit.class).to eq(Octokit::Client)
     end
 
   end
@@ -68,7 +68,7 @@ describe Love do
 
     it 'should run bundler to check current runtime and add gems from there' do
       pending
-      Bundler.should_receive(:load)
+      expect(Bundler).to receive(:load)
       Love.parse_gemfile
     end
 
@@ -78,13 +78,13 @@ describe Love do
 
     it 'should trigger html generation if for site' do
       Love.config ['site']
-      Love::Share.should_receive(:html)
+      expect(Love::Share).to receive(:html)
       Love.share_love
     end
 
     it 'should trigger html generation if for gem' do
       Love.config
-      Love::Share.should_receive(:md)
+      expect(Love::Share).to receive(:md)
       Love.share_love
     end
 
